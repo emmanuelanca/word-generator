e61bcc27a87c861116a155e3e678f41c31a5985a
Emmanuel Anca <anca.emmanuel96@gmail.com>
2024-07-13 16:14:18 -0300

    New example with rewrite rules

--- pseudolatin.py
+++ pseudolatin.py
@@ -2,14 +2,36 @@
 import word_generator
 
 v = ("a", "e", "i", "o", "u",
-     "aa", "ee", "ii", "oo", "uu")
+     "aa", "ee", "ii", "oo", "uu",
+     "ae")
 c = ("b", "s", "d", "f", "g", "h", "c", "l", "m", "n",
-     "p", "r", "t", "v", "i", "qu")
-cc = ("bb", "ss", "dd", "ff", "gg", "cc", "ll", "mm", "n")
+     "p", "r", "t", "i", "qu")
+cc = ("bb", "ss", "dd", "ff", "cc", "ll", "mm", "n")
+r = ("r", "l")
+z = ("a", "us", "er", "um", "ii")
+
 
 patterns = (
-    ((c, ""), v, ("s", "m", "l"), (c, cc), ("as", "us", "um", "ii")),
+    (c, (r, ""), v, (c, cc), z),
 )
 
-generator = word_generator.Generator(patterns)
+
+rewrite_rules = {
+    r"(?<=i)ii": "aa",
+    r"mr": "br",
+    r"nr": "t",
+    r"hr": "fr",
+    r"lr": "dr",
+    r"rr": "r",
+    r"sr": "s",
+    r"sl": "s",
+    r"dl": "dr",
+    r"ml": "bl",
+    r"nl": "t",
+    r"hl": "fl",
+    r"rl": "ll"
+}
+
+
+generator = word_generator.Generator(patterns, rewrite_rules)
 print(generator.generate())
